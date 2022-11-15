f99d22fcb3aab4b702ff9877bcf337e424f6f09c
Nicholas <nicholaswu498@gmail.com>
2022-11-15 21:10:03 +1300

    modified to only fetch once

--- src/pages/StockSearch.js
+++ src/pages/StockSearch.js
@@ -1,37 +1,45 @@
 import React, { useEffect, useState } from "react"
-
+let done = 0;
+var arr = [];
 function StockSearch(props){
 
     const [value, setValue] = useState('');
     const [result, setResult] = useState([]);
+    
+    
 
     useEffect(()=>{
-        if (value.length>0){
+        console.log('first: ' + done)
+        if (done === 0){
             fetch("https://personalbackendreact.azurewebsites.net/gjsgj20ujsa0dfjfbv0dgbjdfiugj459yo").then(
                 response => response.json()
             ).then((response)=>{
+                for (var i in response){
+                    arr.push(response[i]);
+                }
+            })
+            done = 1;
+        }
+
+        if (value.length>0) {
                 setResult([]);
                 let query = value.toLowerCase();
-                for (const element in response){
-                    let val = response[element].sponsorName.toLowerCase();
+                console.log("query: " + query)
+                for (const element in arr){
+                    let val = arr[element].sponsorName.toLowerCase();
+                    console.log("value: " + val)
                     if (val.slice(0, query.length).indexOf(query) !== -1){
                         setResult(prevResult =>{
-                            return [...prevResult, response[element].sponsorName]
+                            console.log(arr[element].sponsorName)
+                            return [...prevResult, arr[element].sponsorName]
                         });
                     }
                 }
-            })
-        }else{
+        } else {
             setResult([])
         }
         
       }, [value])
-    
-    //   handleSubmit(event) {
-    //     alert('A name was submitted: ' + this.state.value);
-    //     event.preventDefault();
-    //     console.log(this.state.value)
-    //   }
 
         return(
           <form>
