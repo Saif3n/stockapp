ebba2a41d7f8f31a0ad1b97ad046934f900b8762
Nicholas <nicholaswu498@gmail.com>
2022-11-16 13:11:32 +1300

    fixed promise await

--- src/pages/StockSearch.js
+++ src/pages/StockSearch.js
@@ -2,72 +2,76 @@ import React, { useEffect, useState } from "react"
 let done = 0;
 var arr = [];
 
-function StockSearch(props){
+
+
+
+function StockSearch(props) {
 
     const [value, setValue] = useState('');
     const [result, setResult] = useState([]);
 
-    useEffect(()=>{
-        console.log("test")
-        if (done === 0){
+    useEffect(() => {
+
+        
+        if (done === 0) {
             fetch("https://personalbackendreact.azurewebsites.net/gjsgj20ujsa0dfjfbv0dgbjdfiugj459yo").then(
                 response => response.json()
-            ).then((response)=>{
-                for (var i in response){
+            ).then((response) => {
+                for (var i in response) {
                     arr.push(response[i]);
                 }
-                for (const element in arr){
-                    setResult(prevResult =>{
+                for (const element in arr) {
+                    setResult(prevResult => {
                         return [...prevResult, arr[element]]
                     });
-        
                 }
-            })
 
+            })
         }
 
-        if (value.length>0) {
-                setResult([]);
-                let query = value.toLowerCase();
-     
-                for (const element in arr){
-                    let val = arr[element].sponsorName.toLowerCase();
+        if (value.length > 0) {
+            setResult([]);
+            let query = value.toLowerCase();
 
-                    if (val.slice(0, query.length).indexOf(query) !== -1){
-                        setResult(prevResult =>{
-                            return [...prevResult, arr[element]]
-                        });
-                    }
+            for (const element in arr) {
+                let val = arr[element].sponsorName.toLowerCase();
+
+                if (val.slice(0, query.length).indexOf(query) !== -1) {
+                    setResult(prevResult => {
+                        return [...prevResult, arr[element]]
+                    });
                 }
+            }
+
         } else {
             setResult([]);
-                for (const element in arr){
-                        setResult(prevResult =>{
-                            return [...prevResult, arr[element]]
-                        });
-        
-                }
-            // setResult([])
-        }
-        
-      }, [value])
+            for (const element in arr) {
+                setResult(prevResult => {
+                    return [...prevResult, arr[element]]
+                });
 
-        return(
-          <form onLoad={(event) => setValue("")}>
+            }
+
+        }
+        done = 1;
+    }, [value])
+
+    return (
+        <form onLoad={(event) => setValue("")}>
             <label>
-              Name:
-              <input type="text" onChange={(event) => setValue(event.target.value)} value={value}/>
+                Name:
+                <input type="text" onChange={(event) => setValue(event.target.value)} value={value} />
             </label>
             <div className="w">
-                {result.map((result, index) =>(
+                {result.map((result, index) => (
                     <div key={index} className="searchEntry">
-                        {result.teamName}
-                        {result.sponsorName}
-                        
+                        <li>{result.teamName}</li>
+                        <li>{result.sponsorName}</li>
+
                     </div>
                 ))}
             </div>
-          </form>
-        );
-        }
+        </form>
+    );
+}
 export default StockSearch;
