c5d2856ec8cbf77de324e43397c1803d371ba988
Nicholas <nicholaswu498@gmail.com>
2022-11-19 16:14:57 +1300

    successfully implemented dropdown menu

--- src/pages/StockSearch.js
+++ src/pages/StockSearch.js
@@ -4,49 +4,41 @@ import DropdownButton from 'react-bootstrap/DropdownButton';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 let done = 0;
+let sponsor = 0;
 let arr = [];
+let defArr = [];
 let teamArr = [];
 
 function StockSearch(props) {
 
     const [value, setValue] = useState('');
-    const[option, setOption] = useState('');
+    const [option, setOption] = useState('');
     const [result, setResult] = useState([]);
     const [team, setTeam] = useState([]);
 
     useEffect(() => {
-        console.log("The chosen value is: " + option);
         if (done === 0) {
             fetch("https://personalbackendreact.azurewebsites.net/gjsgj20ujsa0dfjfbv0dgbjdfiugj459yo").then(
                 response => response.json()
             ).then((response) => {
                 teamArr.push('<-- Select a team -->')
-                for (var i in response) {
+                for (const i in response) {
                     arr.push(response[i]);
-                    // gets array of teams for dropdown
-                    
+                    // gets array of teams for dropdown 
                     if (!teamArr.includes(response[i].teamName)) {
                         teamArr.push(response[i].teamName);
                     }
-                }
-                for (const element in arr) {
                     setResult(prevResult => {
-                        return [...prevResult, arr[element]]
+                        return [...prevResult, arr[i]]
                     });
                 }
-
-                for (const element in teamArr) {
-                    setTeam(prevResult => {
-                        return [...prevResult, teamArr[element]]
-                    });
-                }
-
-
+                defArr = arr;
             })
         }
 
         if (value.length > 0) {
             setResult([]);
+
             let query = value.toLowerCase();
 
             for (const element in arr) {
@@ -60,22 +52,33 @@ function StockSearch(props) {
             }
 
         } else {
-            setResult([]);
-            for (const element in arr) {
-                setResult(prevResult => {
-                    return [...prevResult, arr[element]]
-                });
-
-            }
-
+            setResult(defArr);
         }
         done = 1;
 
     }, [value])
 
-    const handleChange = e =>{
-        console.log(e.target.value)
+    const handleDropdownChange = e => {
+
         setOption(e.target.value);
+        sponsor = 1;
+        setResult([])
+
+        let query = e.target.value.toLowerCase();
+        console.log(query)
+        if (e.target.value === '<-- Select a team -->') {
+            setResult(defArr);
+        } else {
+            for (const element in arr) {
+                let val = arr[element].teamName.toLowerCase();
+                if (val.slice(0, query.length).indexOf(query) !== -1) {
+                    setResult(prevResult => {
+                        return [...prevResult, arr[element]]
+                    });
+                }
+            }
+        }
+
     }
     return (
         <form>
@@ -83,17 +86,17 @@ function StockSearch(props) {
                 Name:
                 <input type="text" onChange={(event) => setValue(event.target.value)} value={value} />
             </label>
-            
 
-            
+
+
             <div className="w">
-                <select id="dropdown-basic-button" title="Dropdown button" onChange={handleChange}>
-                {team.map((team, index) => (
-                        <option className={'op'+team} key={index}>{team}</option>
-                ))}
+                <select id="dropdown-basic-button" title="Dropdown button" onChange={handleDropdownChange}>
+                    {teamArr.map((teamArr, index) => (
+                        <option className={'op' + teamArr} key={index}>{teamArr}</option>
+                    ))}
                 </select>
             </div>
-            
+
             <div className="w">
                 {result.map((result, index) => (
                     <ul key={index} className={result.teamName}>
