fdd3ea5ac1faebaf4a24b964af6083b97e99d338
Nicholas <nicholaswu498@gmail.com>
2022-12-12 23:06:43 +1300

    saving project

--- src/pages/LineGraph.js
+++ src/pages/LineGraph.js
@@ -13,8 +13,7 @@ const LineGraph = React.forwardRef((props, ref) => {
   const name = props.stockName;
   console.log(name)
   const polyLineData = [];
-  const fetchPromise = fetch("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=" + name + "&outputsize=full&apikey=" + process.env.STOCK_API);
-
+  let fetchPromise = '';
 
 
 
@@ -24,6 +23,8 @@ const LineGraph = React.forwardRef((props, ref) => {
   const height = 300;
 
   useEffect(() => {
+
+    fetchPromise = fetch("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=" + name + "&outputsize=full&apikey=" + process.env.STOCK_API);
 
     fetchPromise.then(response => response.json()).then((response) => {
 
@@ -44,64 +45,69 @@ const LineGraph = React.forwardRef((props, ref) => {
       setDataTest(polyLineData);
 
       const xCoords = dataTest.filter((d, i) => i % 2 === 0);
-    const yCoords = dataTest.filter((d, i) => i % 2 !== 0);
-    const coords = d3.zip(xCoords, yCoords)
-      .map(d => ({ x: d[0], y: d[1] }));
+      const yCoords = dataTest.filter((d, i) => i % 2 !== 0);
+      const coords = d3.zip(xCoords, yCoords)
+        .map(d => ({ x: d[0], y: d[1] }));
 
 
-    const margin = { top: 20, right: 20, bottom: 30, left: 50 };
+      const margin = { top: 20, right: 20, bottom: 30, left: 50 };
 
-    const xScale = d3.scaleLinear()
-      .domain([0, d3.max(coords, d => d.x)])
-      .range([margin.left, width - margin.right]);
+      const xScale = d3.scaleLinear()
+        .domain([0, d3.max(coords, d => d.x)])
+        .range([margin.left, width - margin.right]);
 
-    // changes accordingly when entering into stocksearc
-    const testParse = parseInt(d3.max(coords, d => d.y)) + 100;
-    const yScale = d3.scaleLinear()
-      .domain([40, testParse])
-      .range([height - margin.bottom, margin.top]);
+      // changes accordingly when entering into stocksearc
+      const testParse = parseInt(d3.max(coords, d => d.y)) + 100;
+      const yScale = d3.scaleLinear()
+        .domain([40, testParse])
+        .range([height - margin.bottom, margin.top]);
 
-    const line = d3.line()
-      .x(d => xScale(d.x))
-      .y(d => yScale(d.y));
-
-
-
-    const svg = d3.select(svgRef.current);
-    // svg.append("g")
-    //   .attr("transform", `translate(0, ${height - margin.bottom})`)
-    //   .call(d3.axisBottom(xScale).tickSize(0));
-
-    svg.append("rect")
-      .attr("x", margin.left)
-      .attr("y", margin.top)
-      .attr("width", width - margin.left - margin.right)
-      .attr("height", height - margin.top - margin.bottom)
-      .attr("stroke", "black")
-      .attr("fill", "none");
+      const line = d3.line()
+        .x(d => xScale(d.x))
+        .y(d => yScale(d.y));
 
 
-    svg.append("g")
-      .attr("transform", `translate(${margin.left}, 0)`)
-      .call(d3.axisLeft(yScale));
-    svg.append("path")
-      .datum(coords)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-linecap", "round")
-      .attr("stroke-width", 1.5)
-      .attr("d", line);
-      
-    });
+
+      const svg = d3.select(svgRef.current);
+      // svg.append("g")
+      //   .attr("transform", `translate(0, ${height - margin.bottom})`)
+      //   .call(d3.axisBottom(xScale).tickSize(0));
+
+      svg.append("rect")
+        .attr("x", margin.left)
+        .attr("y", margin.top)
+        .attr("width", width - margin.left - margin.right)
+        .attr("height", height - margin.top - margin.bottom)
+        .attr("stroke", "black")
+        .attr("fill", "none");
 
 
-    
+      svg.append("g")
+        .attr("transform", `translate(${margin.left}, 0)`)
+        .call(d3.axisLeft(yScale));
+      svg.append("path")
+        .datum(coords)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("d", line);
+
+
+
+
+    })
+
+
+
+
+
   }, [fetchPromise, dataTest]);
 
   return (
     <svg ref={svgRef} width={width} height={height} />
   );
-});
+})
 
 export default LineGraph;
