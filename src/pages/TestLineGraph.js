0c484c43dd229d7c63f6bff01737553120fd6f7d
Nicholas <nicholaswu498@gmail.com>
2023-01-03 18:18:18 +1300

    implemented vertical line on hover

--- src/pages/TestLineGraph.js
+++ src/pages/TestLineGraph.js
@@ -28,7 +28,7 @@ const TestLineGraph = React.forwardRef((props, ref) => {
                 const dateOfElement = new Date(element);
 
                 if (dateOfElement > firstRaceDate && dateOfElement < lastRaceDate) {
-                    const closePrice = parseInt(response['Time Series (Daily)'][element]['4. close']);
+                    const closePrice = parseFloat(response['Time Series (Daily)'][element]['4. close']);
                     polyLineData.push({ x: dateOfElement, y: closePrice });
                 }
             }
@@ -50,6 +50,8 @@ const TestLineGraph = React.forwardRef((props, ref) => {
 
 
             const svg = d3.select(svgRef.current);
+            const yMin = yScale.domain()[0];
+            const yMax = yScale.domain()[1];
 
 
             svg.append('rect')
@@ -67,8 +69,8 @@ const TestLineGraph = React.forwardRef((props, ref) => {
             svg.append('g')
                 .attr('transform', `translate(0, ${height - margin.bottom})`)
                 .call(d3.axisBottom(xScale)
-                    .ticks(d3.timeDay.every(1))
-                    .tickFormat(d3.timeFormat('%d-%m-%Y')));
+                    .ticks(d3.timeDay.every(20))
+                    .tickFormat(d3.timeFormat('%m-%d')));
 
 
             svg.append('path')
@@ -80,12 +82,37 @@ const TestLineGraph = React.forwardRef((props, ref) => {
                 .attr('stroke-width', 1.5)
                 .attr('d', line);
 
+            const numTicks = 10;
+            const xTickValues = xScale.ticks(numTicks);
+
+            svg.selectAll('.vertical-gridline')
+                .data(xTickValues)
+                .enter()
+                .append('line')
+                .attr('class', 'vertical-gridline')
+                .attr('x1', d => xScale(d))
+                .attr('y1', yScale(yMin))
+                .attr('x2', d => xScale(d))
+                .attr('y2', yScale(yMax))
+                .attr('stroke', 'gray')
+                .attr('stroke-dasharray', '3,3');
+
             polyLineData.forEach(point => {
                 svg.append('circle')
                     .attr('cx', xScale(point.x))
                     .attr('cy', yScale(point.y))
-                    .attr('r', 3)
+                    .attr('r', 2)
                     .attr('fill', 'blue')
+
+                svg.append('line')
+                    .attr('x1', xScale(point.x))
+                    .attr('y1', yScale(yMin))
+                    .attr('x2', xScale(point.x))
+                    .attr('y2', yScale(yMax))
+                    .attr('stroke', 'white')
+                    .attr('stroke-width', 5)
+                    .attr('stroke-opacity', 0)
+                    .attr('class', 'existing-line')
                     .on('mouseover', () => {
                         svg.append('text')
                             .attr('x', xScale(point.x))
@@ -95,21 +122,35 @@ const TestLineGraph = React.forwardRef((props, ref) => {
                             .text(point.y)
                             .attr('fill', 'green')
                             .style('font-weight', 'bold');
+
+                        // Append vertical line to SVG
+                        svg.append('line')
+                            .attr('x1', xScale(point.x))
+                            .attr('y1', yScale(yMin))
+                            .attr('x2', xScale(point.x))
+                            .attr('y2', yScale(yMax))
+                            .attr('stroke', 'green')
+                            .attr('stroke-width', 2)
+                            .attr('stroke-dasharray', '3,3')  // Dotted line
+                            .attr('class', 'mouseover-line');
                     })
-                    .on('mouseout', () => {
-                        svg.select('.mouseover-text').remove();
-                    });
-            });
-
-
+                .on('mouseout', () => {
+                    svg.select('.mouseover-text').remove();
+                    svg.select('.mouseover-line').remove();
+                });
 
 
         });
-    }, []);
 
-    return (
-        <svg ref={svgRef} width={width} height={height} />
-    );
+
+
+
+    });
+}, []);
+
+return (
+    <svg ref={svgRef} width={width} height={height} />
+);
 });
 
 export default TestLineGraph;
