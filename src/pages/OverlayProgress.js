6b22274608116a19da5819d7665dfcf246ec331b
Nicholas <nicholaswu498@gmail.com>
2023-02-19 17:19:56 +1300

    removed ugly footer in overlay

--- src/pages/OverlayProgress.js
+++ src/pages/OverlayProgress.js
@@ -30,11 +30,13 @@ const OverlayProgress = React.forwardRef((props, ref) => {
                         Please note, this website is still heavily under development. You should consider this a minimum viable product. <br></br><br></br> (Most of the stock related-SVG elements + backend logic, but I've yet to finish the front-end of this website - it will look vastly different to how it looks right now, so watch this space!)
                         <br></br>
                         <br></br>
+                        <div>My endpoints are hosted on Microsoft Azure, after a period of inactivity, the endpoints may take a while to 'wake up' again.</div>
+                        <br></br>
+                        <br></br>
                         Last updated on: 15/01/2023
                         {isLoading ? <LoadingSpinner /> : <span className="close-button" onClick={() => setShowOverlay(false)}>Click to view website!</span>}
-               
+
                     </div>
-                    <div className="footer">My endpoints are hosted on Microsoft Azure, after a period of inactivity, the endpoints may take a while to 'wake up' again.</div>
 
                 </div>
             ) : null
